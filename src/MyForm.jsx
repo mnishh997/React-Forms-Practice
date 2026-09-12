cc62dafe1e50d455eb4bac18ce65f1bfebf7cfc8
Manish Kumar <mnishh997@gmail.com>
2026-09-12 12:03:13 +0530

    basic form

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -1,8 +1,23 @@
 export default function MyForm() {
   return (
-    <form>
-      <label>Name: </label>
-      <input type="text" />
-    </form>
+    <>
+      <section>
+        <h1>Sign Up Form</h1>
+      </section>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="joe@email.com"
+        />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
