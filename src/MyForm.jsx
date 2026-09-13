56633794fa3313240cc00a5e6da36b93320861cd
Manish Kumar <mnishh997@gmail.com>
2026-09-13 12:21:25 +0530

    Form Data fetched using action method

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -1,18 +1,25 @@
 export default function MyForm() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formEl = event.currentTarget;
-    const formData = new FormData(formEl);
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const formEl = event.currentTarget;
+  //   const formData = new FormData(formEl);
+  //   const email = formData.get("email");
+  //   console.log(email);
+  //   formEl.reset();
+  // }
+
+  function signUp(formData) {
     const email = formData.get("email");
+    const password = formData.get("password");
     console.log(email);
-    formEl.reset();
+    console.log(password);
   }
 
   return (
     <section>
       <h1>Sign Up Form</h1>
 
-      <form method="POST" onSubmit={handleSubmit}>
+      <form action={signUp}>
         <label htmlFor="email">Email:</label>
         <input
           type="email"
