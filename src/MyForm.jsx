41550c6b15fc92f4761353f91e3461d6bb3c709f
Manish Kumar <mnishh997@gmail.com>
2026-09-13 12:12:49 +0530

    Form data fetch applied

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -1,10 +1,18 @@
 export default function MyForm() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget;
+    const formData = new FormData(formEl);
+    const email = formData.get("email");
+    console.log(email);
+    formEl.reset();
+  }
+
   return (
-    <>
-      <section>
-        <h1>Sign Up Form</h1>
-      </section>
-      <form>
+    <section>
+      <h1>Sign Up Form</h1>
+
+      <form method="POST" onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
         <input
           type="email"
@@ -18,6 +26,6 @@ export default function MyForm() {
         <br />
         <button>Submit</button>
       </form>
-    </>
+    </section>
   );
 }
